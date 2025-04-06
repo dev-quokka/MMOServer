662757726e473634a6db919ae88ea83a3a4f8b78
dev-quokka <rlaehdcks1010@gmail.com>
2025-04-06 20:14:35 +0900

    Add Channel Server Stucture

--- CenterServer/InGameManager/ChannelManager/ChannelManager.h
+++ CenterServer/InGameManager/ChannelManager/ChannelManager.h
@@ -13,19 +13,10 @@ class InGameUser;
 class ChannelManager {
 public:
 	~ChannelManager() {
-		channelUserCountSyncThreadRun = false;
-		if (channelUserCountSyncThread.joinable()) {
-			channelUserCountSyncThread.join();
-		}
+
 	}
 
 private:
 	// 80 bytes
 	std::unordered_map<uint16_t, Room*> channelMap;
-
-	// 16 byte
-	std::thread channelUserCountSyncThread;
-
-	// 1 byte
-	std::atomic<bool> channelUserCountSyncThreadRun = false;
 };
